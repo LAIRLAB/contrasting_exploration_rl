4a0ce3c0f6007507746e458061329720ace86aa8
Anirudh Vemula <vvanirudh@gmail.com>
2018-09-06 13:40:35 -0400

    Decreased number of workers

--- ars/test/tune_lqr_ars.py
+++ ars/test/tune_lqr_ars.py
@@ -14,7 +14,7 @@ def main():
     parser.add_argument('--deltas_used', '-du', type=int, default=8)
     parser.add_argument('--step_size', '-s', type=float, default=0.02)
     parser.add_argument('--delta_std', '-std', type=float, default=.03)
-    parser.add_argument('--n_workers', '-e', type=int, default=18)
+    parser.add_argument('--n_workers', '-e', type=int, default=2)
     parser.add_argument('--rollout_length', '-r', type=int, default=10)
     parser.add_argument('--shift', type=float, default=0)
     parser.add_argument('--seed', type=int, default=237)
