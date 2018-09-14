0188b6a682092dc8358bef631bbb4a7ca508603d
Anirudh Vemula <vvanirudh@gmail.com>
2018-09-14 13:28:24 -0400

    Changed tuning candidates

--- exact/test/tune_lqr_exact.py
+++ exact/test/tune_lqr_exact.py
@@ -39,7 +39,7 @@ def main():
 
     ray.init()
 
-    stepsizes = [5e-6, 1e-5, 5e-5, 1e-4, 5e-4, 1e-3, 5e-3]
+    stepsizes = [5e-4, 1e-3, 5e-3, 1e-2, 5e-2, 1e-1]
     num_directions = [1]
     num_top_directions = [1]
     perturbations = [1e-4]
