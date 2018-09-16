56a7eac2ccacd4c535f22d75b89941f74a8f84de
Anirudh Vemula <vvanirudh@gmail.com>
2018-09-16 15:23:06 -0400

    No parallelization

--- ars/test/tune_lqr_ars.py
+++ ars/test/tune_lqr_ars.py
@@ -76,12 +76,12 @@ def main():
                             params['delta_std'] = p
                             print('Seed: %d, Horizon: %d, Step Size: %f, Num directions: %d, Used directions: %d, Perturbation: %f' % (seed, h, s, nd, ntd, p))
                             if not infeasible:                                    
-                                result_table[c] = run_ars.remote(params)
+                                result_table[c] = ray.get(run_ars.remote(params))
                             else:
                                 result_table[c] = ray.put(float('inf'))
                             c += 1
                         infeasible = False
-                        result_table[prev_c:c] = ray.get(result_table[prev_c:c])
+                        # result_table[prev_c:c] = ray.get(result_table[prev_c:c])
                         print(result_table[prev_c:c])
                         prev_c = c
 
