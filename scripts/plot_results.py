038319fc56e95e5b1e91075bbbd4aab4aab4ad18
Anirudh Vemula <vvanirudh@gmail.com>
2018-09-26 15:42:55 -0400

    Corrected plot script

--- scripts/plot_results.py
+++ scripts/plot_results.py
@@ -7,8 +7,8 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--env_name', type=str, default='Swimmer-v2')
 parser.add_argument('--h_start', type=int, default=1)
-parser.add_argument('--h_end', type=int, default=21)
-parser.add_argument('--h_bin', type=int, default=2)
+parser.add_argument('--h_end', type=int, default=11)
+parser.add_argument('--h_bin', type=int, default=1)
 
 args = parser.parse_args()
 params = vars(args)
@@ -28,10 +28,10 @@ mean_exact = np.mean(exact_results, axis=0)
 std_exact = np.std(exact_results, axis=0) / np.sqrt(exact_results.shape[0])
 
 plt.plot(horizons, mean_ars, color='red', label='ARS', linewidth=2)
-plt.fill_between(horizons, np.maximum(0, mean_ars - std_ars), np.minimum(1e8, mean_ars + std_ars), facecolor='red', alpha=0.2)
+plt.fill_between(horizons, mean_ars - std_ars, mean_ars + std_ars, facecolor='red', alpha=0.2)
 
 plt.plot(horizons, mean_exact, color='blue', label='ExAct', linewidth=2)
-plt.fill_between(horizons, np.maximum(0, mean_exact - std_exact), np.minimum(1e8, mean_exact + std_exact), facecolor='blue', alpha=0.2)
+plt.fill_between(horizons, mean_exact - std_exact, mean_exact + std_exact, facecolor='blue', alpha=0.2)
 
 plt.legend()
 
