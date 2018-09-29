28e3d54eaa33d3235419f2260129614064c9c2b4
Anirudh Vemula <vvanirudh@gmail.com>
2018-09-29 12:55:59 -0400

    Plots done

--- scripts/plot_lqr_results.py
+++ scripts/plot_lqr_results.py
@@ -5,33 +5,38 @@ import pickle
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--h_start', type=int, default=1)
-parser.add_argument('--h_end', type=int, default=21)
-parser.add_argument('--h_bin', type=int, default=2)
-
 args = parser.parse_args()
 params = vars(args)
 
-ars_filename = 'data/ars_results_lqr_'+ str(args.h_start) + '_' + str(args.h_end) + '_' + str(args.h_bin) +'.pkl'
-exact_filename = 'data/exact_results_lqr_'+ str(args.h_start) + '_' + str(args.h_end) + '_' + str(args.h_bin) +'.pkl'
+ars_filename = 'saved_data/ars_results_lqr.pkl'
 
-horizons = list(range(args.h_start, args.h_end, args.h_bin))
+noise_cov = [1e-4, 5e-4, 1e-3, 5e-3, 1e-2, 5e-2, 1e-1, 5e-1]
+
 
 ars_results = pickle.load(open(ars_filename, 'rb'))
-exact_results = pickle.load(open(exact_filename, 'rb'))
 
 mean_ars = np.mean(ars_results, axis=0)
 std_ars = np.std(ars_results, axis=0) / np.sqrt(ars_results.shape[0])
 
-mean_exact = np.mean(exact_results, axis=0)
-std_exact = np.std(exact_results, axis=0) / np.sqrt(exact_results.shape[0])
+mean_ars = mean_ars / 1e4
+std_ars = std_ars / 1e4
 
-plt.plot(horizons, mean_ars, color='red', label='ARS', linewidth=2)
-plt.fill_between(horizons, np.maximum(0, mean_ars - std_ars), np.minimum(1e8, mean_ars + std_ars), facecolor='red', alpha=0.2)
+#plt.rc('text', usetex=True)
+#plt.rc('font', family='serif')
 
-plt.plot(horizons, mean_exact, color='blue', label='ExAct', linewidth=2)
-plt.fill_between(horizons, np.maximum(0, mean_exact - std_exact), np.minimum(1e8, mean_exact + std_exact), facecolor='blue', alpha=0.2)
+plt.plot(noise_cov, mean_ars, color='red', label='ARS', linewidth=2)
+plt.fill_between(noise_cov, np.maximum(0, mean_ars - std_ars), np.minimum(1e6, mean_ars + std_ars), facecolor='red', alpha=0.2)
+
+plt.xscale('log')
+plt.yscale('linear')
+plt.xlabel('Standard deviation of the noise in LQR dynamics')
+plt.ylabel('Number of samples needed (in multiples of $10^4$)')
+plt.title('Plot of number of samples needed vs standard deviation \nof noise in LQR dynamics')
 
 plt.legend()
 
-plt.show()
+filename = 'plt_lqr'
+plt.savefig('plot/'+filename+'.pdf', format='pdf')
+plt.savefig('plot/'+filename+'.png', format='png')
+
+# plt.show()
--- scripts/plot_zoomed_results.py
+++ scripts/plot_zoomed_results.py
@@ -7,41 +7,31 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--env_name', type=str, default='Swimmer-v2')
 parser.add_argument('--h_start', type=int, default=1)
-parser.add_argument('--h_end', type=int, default=202)
-parser.add_argument('--h_bin', type=int, default=20)
+parser.add_argument('--h_end', type=int, default=16)
+parser.add_argument('--h_bin', type=int, default=1)
 parser.add_argument('--h_start_z', type=int, default=1)
-parser.add_argument('--h_end_z', type=int, default=11)
+parser.add_argument('--h_end_z', type=int, default=6)
 parser.add_argument('--h_bin_z', type=int, default=1)
 
 args = parser.parse_args()
 params = vars(args)
 
-ars_filename = 'data/ars_results_'+params['env_name']+'_'+ str(args.h_start) + '_' + str(args.h_end) + '_' + str(args.h_bin) +'.pkl'
-exact_filename = 'data/exact_results_' + params['env_name']+'_'+ str(args.h_start) + '_' + str(args.h_end) + '_' + str(args.h_bin) +'.pkl'
-
-ars_filename_z = 'data/ars_results_'+params['env_name']+'_'+ str(args.h_start_z) + '_' + str(args.h_end_z) + '_' + str(args.h_bin_z) +'.pkl'
-exact_filename_z = 'data/exact_results_' + params['env_name']+'_'+ str(args.h_start_z) + '_' + str(args.h_end_z) + '_' + str(args.h_bin_z) +'.pkl'
+ars_filename = 'saved_data/ars_results_'+params['env_name']+'_'+ str(args.h_start) + '_' + str(args.h_end) + '_' + str(args.h_bin) +'.pkl'
+exact_filename = 'saved_data/exact_results_' + params['env_name']+'_'+ str(args.h_start) + '_' + str(args.h_end) + '_' + str(args.h_bin) +'.pkl'
 
 horizons = list(range(args.h_start, args.h_end, args.h_bin))
 horizons_z = list(range(args.h_start_z, args.h_end_z, args.h_bin_z))
 
+num_z = len(horizons_z)
+
 ars_results = pickle.load(open(ars_filename, 'rb'))
 exact_results = pickle.load(open(exact_filename, 'rb'))
-
-ars_results_z = pickle.load(open(ars_filename_z, 'rb'))
-exact_results_z = pickle.load(open(exact_filename_z, 'rb'))
 
 mean_ars = np.mean(ars_results, axis=0)
 std_ars = np.std(ars_results, axis=0) / np.sqrt(ars_results.shape[0])
 
 mean_exact = np.mean(exact_results, axis=0)
 std_exact = np.std(exact_results, axis=0) / np.sqrt(exact_results.shape[0])
-
-mean_ars_z = np.mean(ars_results_z, axis=0)
-std_ars_z = np.std(ars_results_z, axis=0) / np.sqrt(ars_results_z.shape[0])
-
-mean_exact_z = np.mean(exact_results_z, axis=0)
-std_exact_z = np.std(exact_results_z, axis=0) / np.sqrt(exact_results_z.shape[0])
 
 fig, ax = plt.subplots()
 
@@ -52,24 +42,49 @@ ax.plot(horizons, mean_exact, color='blue', label='ExAct', linewidth=2)
 ax.fill_between(horizons, mean_exact - std_exact, mean_exact + std_exact, facecolor='blue', alpha=0.2)
 
 from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
-axins = zoomed_inset_axes(ax, 10, loc=2)
+zoom = None
+if args.env_name == 'Swimmer-v2':
+    zoom = True
+elif args.env_name == 'HalfCheetah-v2':
+    zoom = False
+else:
+    raise NotImplementedError
+if zoom:
+    
+    axins = zoomed_inset_axes(ax, 1.5, loc=2)
+    
+    axins.plot(horizons_z, mean_ars[:num_z], color='red', label='ARS', linewidth=2)
+    axins.fill_between(horizons_z, mean_ars[:num_z] - std_ars[:num_z], mean_ars[:num_z] + std_ars[:num_z], facecolor='red', alpha=0.2)
 
-axins.plot(horizons_z, mean_ars_z, color='red', label='ARS', linewidth=2)
-axins.fill_between(horizons_z, mean_ars_z - std_ars_z, mean_ars_z + std_ars_z, facecolor='red', alpha=0.2)
+    axins.plot(horizons_z, mean_exact[:num_z], color='blue', label='ExAct', linewidth=2)
+    axins.fill_between(horizons_z, mean_exact[:num_z] - std_exact[:num_z], mean_exact[:num_z] + std_exact[:num_z], facecolor='blue', alpha=0.2)
 
-axins.plot(horizons_z, mean_exact_z, color='blue', label='ExAct', linewidth=2)
-axins.fill_between(horizons_z, mean_exact_z - std_exact_z, mean_exact_z + std_exact_z, facecolor='blue', alpha=0.2)
+#x1, x2, y1, y2 = 1, 5, 0, 5
+#axins.set_xlim(x1, x2)
+#axins.set_ylim(y1, y2)
 
-x1, x2, y1, y2 = 1, 10, 0, 10
-axins.set_xlim(x1, x2)
-axins.set_ylim(y1, y2)
 
-plt.yticks(visible=False)
-plt.xticks(visible=False)
+if zoom:
+    axins.set_ylim(0, 2)
+    plt.yticks(visible=False)
+    plt.xticks(visible=False)
 
-from mpl_toolkits.axes_grid1.inset_locator import mark_inset
-mark_inset(ax, axins, loc1=2, loc2=4, fc="none", ec="0.5")
+if zoom:    
+    from mpl_toolkits.axes_grid1.inset_locator import mark_inset
+    mark_inset(ax, axins, loc1=4, loc2=3, fc="none", ec="0.5")
 
-# plt.legend()
+ax.set_xlabel('Horizon Length H')
+ax.set_ylabel('Mean Return')
 
-plt.show()
+ax.legend(loc='lower right')
+
+title = 'Plot of Mean Return vs Horizon Length H for '+str(params['env_name'])
+ax.set_title(title)
+
+filename = 'plt_'+str(params['env_name'])+'.pdf'
+plt.savefig('plot/'+filename, format='pdf')
+
+filename = 'plt_'+str(params['env_name'])+'.png'
+plt.savefig('plot/'+filename, format='png')
+
+#plt.show()
